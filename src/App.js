de6692a445fbb1c9420e046eb227a9dc0e3aa7ec
“kylemharman” <kylemartinharman@gmail.com>
2019-11-28 21:14:08 +1100

    connected with rails DB

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+import {Router, Link} from '@reach/router';
 import './App.css';
+import Plane from './components/Plane/Plane'
 
 function App() {
   return (
     <div className="App">
      <p>Hello world!!!</p>
+     <Plane />
     </div>
   );
 }
---
+++ src/components/Plane/Plane.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+import Axios from 'axios'
+export default class Plane extends Component {
+    constructor(props) {
+        super(props)
+        this.state = { 
+            airplane: [], 
+            airplaneText:''}
+       
+    }
+
+    componentDidMount() {
+        const SERVER_URL = `http://localhost:3000/planes.json`
+        Axios.get(SERVER_URL) //this? 
+            .then(res => {
+                const airplane = res.data;
+                this.setState({airplane})
+            })    
+        
+        // .then(response => self.setState((state) => ({...state, airplane: response.data})) ); //results => console.log(JSON.stringify(results.data))
+            // console.log(this.state.airplane)
+    }
+
+    
+    
+    render() {
+
+    const name = this.state.airplane.map((e, id)  => <li key={id}>{e.name}</li>)
+        
+        return (
+            <div>
+                <ul>
+                 {name}
+
+                </ul>
+            </div>
+        )
+    }
+}
